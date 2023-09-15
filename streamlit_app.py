e594f0b2286453d462b61d3245953f7223fc7b40
Sriman07 <84221527+Sriman07@users.noreply.github.com>
2023-09-15 21:30:25 +0530

    Update streamlit_app.py
    with fix

--- streamlit_app.py
+++ streamlit_app.py
@@ -56,7 +56,7 @@ streamlit.write('Thanks for adding ', add_my_fruit)
 my_cur.execute("insert into fruit_load_list values ('from streamlit')")
 
 def insert_row_snowflake(new_fruit):
-  with with  my_cnx.cursor() as my_cur:
+  with my_cnx.cursor() as my_cur:
     my_cur.execute("insert into fruit_load_list values ('from streamlit')")
     return "Thanks for adding" + new_fruit
 
